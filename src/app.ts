0218981cd58d90fd442714241b3e336e4760449e
Artem <artem4rolov@gmail.com>
2023-02-21 00:01:00 +0300

    done

--- src/app.ts
+++ src/app.ts
@@ -1,69 +1,103 @@
-(function() {
+// явно определяем тип для всех id
+type ID = string | number;
+
+// интерфейс для тудушки
+interface Todo {
+  userId: ID;
+  id: ID;
+  title: string;
+  completed: boolean;
+}
+
+// интерфейс для юзеров
+interface User {
+  id: ID;
+  name: string;
+}
+
+(function () {
   // Globals
-  const todoList = document.getElementById('todo-list');
-  const userSelect = document.getElementById('user-todo');
-  const form = document.querySelector('form');
-  let todos = [];
-  let users = [];
+  const todoList = document.getElementById("todo-list");
+  const userSelect = document.getElementById("user-todo");
+  const form = document.querySelector("form");
+  let todos: Todo[] = [];
+  let users: User[] = [];
 
   // Attach Events
-  document.addEventListener('DOMContentLoaded', initApp);
-  form.addEventListener('submit', handleSubmit);
+  document.addEventListener("DOMContentLoaded", initApp);
+  // если есть форма, то добавляем прослушиватель события
+  form?.addEventListener("submit", handleSubmit);
 
   // Basic Logic
-  function getUserName(userId) {
+  function getUserName(userId: ID) {
     const user = users.find((u) => u.id === userId);
-    return user.name;
+    return user?.name || "user not found";
   }
-  function printTodo({ id, userId, title, completed }) {
-    const li = document.createElement('li');
-    li.className = 'todo-item';
-    li.dataset.id = id;
+  function printTodo({ id, userId, title, completed }: Todo) {
+    const li = document.createElement("li");
+    li.className = "todo-item";
+    // в dataset (дата атрибут html) может быть записано значение только типа string
+    li.dataset.id = String(id);
     li.innerHTML = `<span>${title} <i>by</i> <b>${getUserName(
       userId
     )}</b></span>`;
 
-    const status = document.createElement('input');
-    status.type = 'checkbox';
+    const status = document.createElement("input");
+    status.type = "checkbox";
     status.checked = completed;
-    status.addEventListener('change', handleTodoChange);
+    status.addEventListener("change", handleTodoChange);
 
-    const close = document.createElement('span');
-    close.innerHTML = '&times;';
-    close.className = 'close';
-    close.addEventListener('click', handleClose);
+    const close = document.createElement("span");
+    close.innerHTML = "&times;";
+    close.className = "close";
+    close.addEventListener("click", handleClose);
 
     li.prepend(status);
     li.append(close);
 
-    todoList.prepend(li);
+    // дополнительно проверяем наличие todoList в приложении
+    if (todoList) {
+      todoList.prepend(li);
+    }
   }
 
-  function createUserOption(user) {
-    const option = document.createElement('option');
-    option.value = user.id;
+  // даем тип аргументу функции (юзер)
+  function createUserOption(user: User) {
+    const option = document.createElement("option");
+    // в dataset (дата атрибут html) может быть записано значение только типа string
+    option.value = String(user.id);
     option.innerText = user.name;
 
-    userSelect.append(option);
+    // проверяем, что userSelect существует
+    if (userSelect) {
+      userSelect.append(option);
+    }
   }
 
-  function removeTodo(todoId) {
+  function removeTodo(todoId: ID) {
     todos = todos.filter((todo) => todo.id !== todoId);
 
-    const todo = todoList.querySelector(`[data-id="${todoId}"]`);
-    todo.querySelector('input').removeEventListener('change', handleTodoChange);
-    todo.querySelector('.close').removeEventListener('click', handleClose);
+    const todo = todoList?.querySelector(`[data-id="${todoId}"]`);
+    if (todo) {
+      // проверяем наличие input и класса .close
+      todo
+        .querySelector("input")
+        ?.removeEventListener("change", handleTodoChange);
+      todo.querySelector(".close")?.removeEventListener("click", handleClose);
 
-    todo.remove();
+      todo.remove();
+    }
   }
 
-  function alertError(error) {
+  // даем тип ошибке как тип глобального объекта Error
+  function alertError(error: Error) {
     alert(error.message);
   }
 
   // Event Logic
   function initApp() {
     Promise.all([getAllTodos(), getAllUsers()]).then((values) => {
+      // поскольку мы явно обозначили типы для Promise ниже, ts дает корректные типы для массива value (результаты промисов)
       [todos, users] = values;
 
       // Отправить в разметку
@@ -71,103 +105,136 @@
       users.forEach((user) => createUserOption(user));
     });
   }
-  function handleSubmit(event) {
+  // даем event'у глобальный тип Event
+  function handleSubmit(event: Event) {
     event.preventDefault();
 
-    createTodo({
-      userId: Number(form.user.value),
-      title: form.todo.value,
-      completed: false,
-    });
+    if (form) {
+      createTodo({
+        userId: Number(form.user.value),
+        title: form.todo.value,
+        completed: false,
+      });
+    }
   }
-  function handleTodoChange() {
-    const todoId = this.parentElement.dataset.id;
-    const completed = this.checked;
+  // явно задаем тип для аргумента (тудушка чекается через input)
+  function handleTodoChange(this: HTMLInputElement) {
+    // проверяем, есть ли родительский элемент (это как минимум body)
+    const parent = this.parentElement;
+    if (parent) {
+      const todoId = this.parentElement?.dataset.id;
+      const completed = this.checked;
 
-    toggleTodoComplete(todoId, completed);
+      todoId && toggleTodoComplete(todoId, completed);
+    }
   }
-  function handleClose() {
-    const todoId = this.parentElement.dataset.id;
-    deleteTodo(todoId);
+  function handleClose(this: Element) {
+    const parent = this.parentElement;
+    if (parent) {
+      const todoId = this.parentElement?.dataset.id;
+      todoId && deleteTodo(todoId);
+    }
   }
 
   // Async logic
-  async function getAllTodos() {
+  // явно говорим о том, что резуьтатом промиса будет массив с тудушками
+  async function getAllTodos(): Promise<Todo[]> {
     try {
       const response = await fetch(
-        'https://jsonplaceholder.typicode.com/todos?_limit=15'
+        "https://jsonplaceholder.typicode.com/todos?_limit=15"
       );
       const data = await response.json();
 
       return data;
     } catch (error) {
-      alertError(error);
+      // проверяем, что ошибка является типом глобального объекта Error
+      if (error instanceof Error) {
+        alertError(error);
+      }
+      // в случае ошибки всегда возвращаем пустой массив
+      // так функция, возвращающая PromiseAll выше, поймет, что одним из аргументов точно является массив тудушек
+      return [];
     }
   }
 
-  async function getAllUsers() {
+  async function getAllUsers(): Promise<User[]> {
     try {
       const response = await fetch(
-        'https://jsonplaceholder.typicode.com/users?_limit=5'
+        "https://jsonplaceholder.typicode.com/users?_limit=5"
       );
       const data = await response.json();
 
       return data;
     } catch (error) {
-      alertError(error);
+      // проверяем, что ошибка является типом глобального объекта Error
+      if (error instanceof Error) {
+        alertError(error);
+      }
+      // в случае ошибки всегда возвращаем пустой массив
+      // так функция, возвращающая PromiseAll выше, поймет, что одним из аргументов точно является массив юзеров
+      return [];
     }
   }
 
-  async function createTodo(todo) {
+  // даем тип аргументу функции (тудушка)
+  async function createTodo(todo: Omit<Todo, "id">) {
     try {
       const response = await fetch(
-        'https://jsonplaceholder.typicode.com/todos',
+        "https://jsonplaceholder.typicode.com/todos",
         {
-          method: 'POST',
+          method: "POST",
           body: JSON.stringify(todo),
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         }
       );
 
-      const newTodo = await response.json();
+      const newTodo: Todo = await response.json();
 
       printTodo(newTodo);
     } catch (error) {
-      alertError(error);
+      // проверяем, что ошибка является типом глобального объекта Error
+      if (error instanceof Error) {
+        alertError(error);
+      }
     }
   }
 
-  async function toggleTodoComplete(todoId, completed) {
+  // задаем типы для аргументов функции
+  async function toggleTodoComplete(todoId: ID, completed: boolean) {
     try {
       const response = await fetch(
         `https://jsonplaceholder.typicode.com/todos/${todoId}`,
         {
-          method: 'PATCH',
+          method: "PATCH",
           body: JSON.stringify({ completed }),
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         }
       );
 
       if (!response.ok) {
-        throw new Error('Failed to connect with the server! Please try later.');
+        throw new Error("Failed to connect with the server! Please try later.");
       }
     } catch (error) {
-      alertError(error);
+      // проверяем, что ошибка является типом глобального объекта Error
+      if (error instanceof Error) {
+        alertError(error);
+      }
     }
   }
 
-  async function deleteTodo(todoId) {
+  // задаем тип для id
+  async function deleteTodo(todoId: ID) {
     try {
       const response = await fetch(
         `https://jsonplaceholder.typicode.com/todos/${todoId}`,
         {
-          method: 'DELETE',
+          method: "DELETE",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         }
       );
@@ -175,10 +242,13 @@
       if (response.ok) {
         removeTodo(todoId);
       } else {
-        throw new Error('Failed to connect with the server! Please try later.');
+        throw new Error("Failed to connect with the server! Please try later.");
       }
     } catch (error) {
-      alertError(error);
+      // проверяем, что ошибка является типом глобального объекта Error
+      if (error instanceof Error) {
+        alertError(error);
+      }
     }
   }
-})()
+})();
